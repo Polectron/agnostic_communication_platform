986a13688042438e0c5bdcd29eec763d1b323214
polectron <polectron@gmail.com>
2021-12-08 23:51:36 +0100

    clear terminal correctly depending on os

--- acp_interface/terminal.py
+++ acp_interface/terminal.py
@@ -1,7 +1,13 @@
 from acp_interface.acp_interface import AbstractInterface
 
 import os
+import platform
 
 class TerminalInterface(AbstractInterface):
     def draw(self):
-        os.system('clear')
+        if platform.system() in ("Linux", "Darwin"):
+            os.system("clear")
+        elif platform.system() == "Windows":
+            os.system("cls")
+        else:
+            raise NotImplementedError()
